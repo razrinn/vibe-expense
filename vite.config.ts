b55ba38a6459fc98f38857b42bc7f9068dacecf8
Ray Azrin Karim <rayazrin19@gmail.com>
2025-05-25 01:55:33 +0700

    tidy up

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,4 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
-  server: {
-    host: "0.0.0.0",
-    port: 8887,
-    strictPort: true,
-    hmr: {
-      host: "0.0.0.0",
-      port: 8886,
-    },
-  },
 });

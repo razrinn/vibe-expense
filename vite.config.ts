96f9cda9d4b0be00897de277e25fceb1308fcfdf
Ray Azrin Karim <rayazrin19@gmail.com>
2025-05-25 15:49:10 +0700

    add cloudflare worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa'
+import { cloudflare } from "@cloudflare/vite-plugin";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate' }), cloudflare()],
 });

4b0ec006cb06d38e4d2b374deaf89dec83a57eaa
Ray Azrin Karim <rayazrin19@gmail.com>
2025-05-28 16:52:49 +0700

    reorder layout

--- src/pages/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import CategoryChart from '../components/analytics/CategoryChart';
 import TrendChart from '../components/analytics/TrendChart';
-import KeyMetrics from '../components/analytics/KeyMetrics';
 import ExpenseFilters from '../components/expenses/ExpenseFilters';
 import { useExpenses } from '../context/ExpenseContext';
 import CategoryBudgetOverview from '../components/budgets/CategoryBudgetOverview';
@@ -12,17 +11,13 @@ const AnalyticsPage: React.FC = () => {
 
   return (
     <div className='space-y-4'>
-      <ExpenseFilters
-        filter={filter}
-        categories={categories}
-        onFilterChange={setFilter}
-      />
-
-      <KeyMetrics summary={summary} />
-
-      <CategoryBudgetOverview includeZeroBudget />
-
       <div className='grid grid-cols-1 lg:grid-cols-2 gap-6'>
+        <ExpenseFilters
+          filter={filter}
+          categories={categories}
+          onFilterChange={setFilter}
+        />
+
         <CategoryChart
           data={summary.byCategory}
           categories={categories}
@@ -35,6 +30,8 @@ const AnalyticsPage: React.FC = () => {
           dateRange={filter.dateRange}
         />
       </div>
+
+      <CategoryBudgetOverview includeZeroBudget />
 
       {Object.keys(summary.byCategory).length === 0 && (
         <div className='text-center py-6'>
